11d84bf8ce1292fb0beca1f840ea59e49e516c35
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2024-12-19 16:32:16 +0530

    Update swapnumbers.js

--- swapnumbers.js
+++ swapnumbers.js
@@ -8,3 +8,21 @@ b = a^b;
 a = a^b
 
 console.log(a, b)
+
+
+function add(a,b) {
+while(b!==0) {
+
+	var carry = a & b;
+  a = a ^ b;
+  b = carry<<1
+} 
+return a
+}
+
+
+function sub(a,b) {
+	return add(a, add(~b,1));
+}
+
+console.log(sub(5,1));
